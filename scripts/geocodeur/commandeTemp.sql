08d7a2637a8ff941a266ac91758d486b4c23ec08
Olivier Dorie <olivier.dorie@ign.fr>
2017-10-09 13:29:16 +0200

    correction doublon position

--- scripts/geocodeur/commandeTemp.sql
+++ scripts/geocodeur/commandeTemp.sql
@@ -8,10 +8,18 @@ CREATE TABLE group_90 AS (SELECT g.pk, g.name, g.municipality_id FROM "group" g,
 DROP TABLE IF EXISTS housenumber_90;
 CREATE TABLE housenumber_90 AS (SELECT h.pk, h.number, h.ordinal, h.parent_id FROM housenumber h, group_90 g WHERE h.parent_id=g.pk AND h.number IS NOT NULL);
 UPDATE housenumber_90 SET ordinal='' WHERE ordinal is null;
+DROP TABLE IF EXISTS position_temp;
+CREATE TABLE position_temp AS (SELECT p.* FROM position p, housenumber_90 h WHERE p.housenumber_id=h.pk);
+ALTER TABLE position_temp ADD COLUMN kind_int integer;
+UPDATE position_temp SET kind_int=CASE kind WHEN 'entrance' THEN 0 WHEN 'building' THEN 1 WHEN 'staircase' THEN 2 WHEN 'unit' THEN 3 WHEN 'parcel' THEN 4 WHEN 'segment' THEN 5 WHEN 'utiliti' THEN 6 WHEN 'area' THEN 7 WHEN 'postal' THEN 8 ELSE 9 END;
+DROP TABLE IF EXISTS position_temp_kind;
+CREATE TABLE position_temp_kind AS 
+(SELECT st_x(p.center) as X, st_y(p.center) as Y, p.housenumber_id, p.kind, p.modified_at  FROM position_temp p, housenumber_90 h WHERE p.housenumber_id=h.pk AND p.kind_int=
+(SELECT min(p2.kind_int) FROM position_temp p2 WHERE p2.housenumber_id=h.pk));
 DROP TABLE IF EXISTS position_90;
 CREATE TABLE position_90 AS 
-(SELECT st_x(p.center) as X, st_y(p.center) as Y, p.housenumber_id  FROM position p, housenumber_90 h WHERE p.housenumber_id=h.pk and st_x(p.center)=
-(select min(st_x(p2.center)) from position p2 where p2.housenumber_id=h.pk group by p2.housenumber_id));
+(SELECT p.* FROM position_temp_kind p WHERE modified_at=
+(SELECT p2.modified_at FROM position_temp_kind p2 WHERE p2.housenumber_id=p.housenumber_id order by modified_at DESC LIMIT 1));
 DROP TABLE IF EXISTS level2point_90;
 CREATE TABLE level2point_90 (streetName text, streetAttribute text, endNumberL text, startNumberL text, endNumberR text, startNumberR text, cityName text, cityAttribute text, cityUniqueId text, X1 text, Y1 text, X2 text, Y2 text, geom text);
 INSERT INTO level2point_90 (streetName, cityName, cityUniqueId, X2, Y2) SELECT h.number||''||h.ordinal||' '||g.name, m.name, m.insee, p.X, p.Y FROM municipality_90 m, group_90 g, housenumber_90 h, position_90 p WHERE p.housenumber_id=h.pk and h.parent_id=g.pk and g.municipality_id=m.pk and g.name is not null and h.number is not null;
--- scripts/geocodeur/export_to_ugc.sh
+++ scripts/geocodeur/export_to_ugc.sh
@@ -19,17 +19,29 @@ echo "\set ON_ERROR_STOP 1" > commandeTemp.sql
 
 echo "DROP TABLE IF EXISTS municipality_${dep};" >> commandeTemp.sql
 echo "CREATE TABLE municipality_${dep} AS (SELECT pk, name, insee FROM municipality WHERE insee LIKE '${dep}%');" >> commandeTemp.sql
+
 echo "DROP TABLE IF EXISTS postcode_${dep};" >> commandeTemp.sql
 echo "CREATE TABLE postcode_${dep} AS (SELECT p.pk, p.code, p.complement, p.municipality_id FROM postcode p, municipality_${dep} m WHERE p.municipality_id=m.pk);" >> commandeTemp.sql
+
 echo "DROP TABLE IF EXISTS group_${dep};" >> commandeTemp.sql
 echo "CREATE TABLE group_${dep} AS (SELECT g.pk, g.name, g.municipality_id FROM \"group\" g, municipality_${dep} m WHERE g.municipality_id=m.pk AND g.name IS NOT NULL);" >> commandeTemp.sql
+
 echo "DROP TABLE IF EXISTS housenumber_${dep};" >> commandeTemp.sql
 echo "CREATE TABLE housenumber_${dep} AS (SELECT h.pk, h.number, h.ordinal, h.parent_id FROM housenumber h, group_${dep} g WHERE h.parent_id=g.pk AND h.number IS NOT NULL);" >> commandeTemp.sql
 echo "UPDATE housenumber_${dep} SET ordinal='' WHERE ordinal is null;" >> commandeTemp.sql
+
+echo "DROP TABLE IF EXISTS position_temp;" >> commandeTemp.sql
+echo "CREATE TABLE position_temp AS (SELECT p.* FROM position p, housenumber_${dep} h WHERE p.housenumber_id=h.pk);" >> commandeTemp.sql
+echo "ALTER TABLE position_temp ADD COLUMN kind_int integer;" >> commandeTemp.sql
+echo "UPDATE position_temp SET kind_int=CASE kind WHEN 'entrance' THEN 0 WHEN 'building' THEN 1 WHEN 'staircase' THEN 2 WHEN 'unit' THEN 3 WHEN 'parcel' THEN 4 WHEN 'segment' THEN 5 WHEN 'utiliti' THEN 6 WHEN 'area' THEN 7 WHEN 'postal' THEN 8 ELSE 9 END;" >> commandeTemp.sql
+echo "DROP TABLE IF EXISTS position_temp_kind;" >> commandeTemp.sql
+echo "CREATE TABLE position_temp_kind AS 
+(SELECT st_x(p.center) as X, st_y(p.center) as Y, p.housenumber_id, p.kind, p.modified_at  FROM position_temp p, housenumber_${dep} h WHERE p.housenumber_id=h.pk AND p.kind_int=
+(SELECT min(p2.kind_int) FROM position_temp p2 WHERE p2.housenumber_id=h.pk));" >> commandeTemp.sql
 echo "DROP TABLE IF EXISTS position_${dep};" >> commandeTemp.sql
 echo "CREATE TABLE position_${dep} AS 
-(SELECT st_x(p.center) as X, st_y(p.center) as Y, p.housenumber_id  FROM position p, housenumber_${dep} h WHERE p.housenumber_id=h.pk and st_x(p.center)=
-(select min(st_x(p2.center)) from position p2 where p2.housenumber_id=h.pk group by p2.housenumber_id));" >> commandeTemp.sql
+(SELECT p.* FROM position_temp_kind p WHERE modified_at=
+(SELECT p2.modified_at FROM position_temp_kind p2 WHERE p2.housenumber_id=p.housenumber_id order by modified_at DESC LIMIT 1));" >> commandeTemp.sql
 
 echo "DROP TABLE IF EXISTS level2point_${dep};" >> commandeTemp.sql
 echo "CREATE TABLE level2point_${dep} (streetName text, streetAttribute text, endNumberL text, startNumberL text, endNumberR text, startNumberR text, cityName text, cityAttribute text, cityUniqueId text, X1 text, Y1 text, X2 text, Y2 text, geom text);" >> commandeTemp.sql
